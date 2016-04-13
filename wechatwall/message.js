3e48c0974e86280354d24002b36baffd509899b3
LCRinesnow <602284158@qq.com>
2016-04-13 16:07:20 +0800

    add message.js debug

--- wechatwall/message.js
+++ wechatwall/message.js
@@ -3,7 +3,7 @@ var http = require('http');
 var qs = require('qs');
 var crypto =require('crypto');
 var url=require('url');
-// var xml2js =require('xml2js');
+var xml2js =require('xml2js');
 
 var TOKEN = 'rinesnow';
 
@@ -50,13 +50,13 @@ var server = http.createServer(function (request,response){
 
 		request.addListener('end',function (){
 			console.log(postdata);
-			// var parseString=require('xml2js').parseString;
-			// parseString(postdata,function (err, result){
-			// 	if(!err){
-			// 		console.log(result);
+			var parseString=xml2js.parseString;
+			parseString(postdata,function (err, result){
+				if(!err){
+					console.log(result);
 					response.end('success');
-			// }
-			// });
+				}
+			});
 		});
 	}
 });
