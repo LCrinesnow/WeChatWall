08e2618a92fff9ac8a2b72c4e4cc494099f859c4
LCRinesnow <602284158@qq.com>
2016-04-13 15:30:35 +0800

    add message.js debug

--- wechatwall/message.js
+++ wechatwall/message.js
@@ -1,6 +1,9 @@
 var PORT = 9529;
 var http = require('http');
 var qs = require('qs');
+var crypto =require('crypto');
+var url=require('url');
+var xml2js =require('xml2js');
 
 var TOKEN = 'rinesnow';
 
@@ -10,7 +13,7 @@ function checkSignature(params,token){
 	//3.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信  return true/false
 
 	var key = [token, params.timestamp,params.nonce].sort().join('');//拼接
-	var sha1 = require('crypto').createHash('sha1');
+	var sha1 = crypto.createHash('sha1');
 	sha1.update(key);
 	return sha1.digest('hex')==params.signature;
 
@@ -18,7 +21,6 @@ function checkSignature(params,token){
 
 var server = http.createServer(function (request,response){
 	//解析URL中的Query部分，用qs模块（npm install qs）将Query解析成json
-	var url=require('url');
 	var query = url.parse(request.url).query;
 	var params = qs.parse(query);
 
@@ -26,12 +28,11 @@ var server = http.createServer(function (request,response){
 	console.log("token-->",TOKEN);
 
 	if(!checkSignature(params,TOKEN)){
-		// console.log("chenggong");
-		// response.end(params.echostr);
+		// response.end(params.echostr);不能乱response.end！否则后面response 用不了了！
 		response.end('signature fail');
 		return;
-	// }else{
-	// 	return;
+	}else{
+		console.log("chenggong");
 	}
 		console.log(request.method+'/////');
 
@@ -40,19 +41,22 @@ var server = http.createServer(function (request,response){
 		response.end(params.echostr);
 	}else{
 	//否则是微信给开发者服务器的POST请求
-	 	// request.setEncoding('utf-8');
 		var postdata = '';
-		// console.log('else'+'/////');
 
 		request.addListener('data',function (postchunk){
 			postdata += postchunk;
-			console.log(postchunk+'/////data');
 		});
 		// console.log(postdata+'dfsdf');
 
 		request.addListener('end',function (){
 			console.log(postdata);
-			response.end('success');
+			var parseString=xml2js.parseString;
+			parseString(postdata,function (err, result){
+				if(!err){
+					console.log(result);
+					response.end('success');
+				}
+			});
 		});
 	}
 });
