d167516f3aa42bb07b54aafc5a728beb71fc5a51
LCRinesnow <602284158@qq.com>
2016-04-17 10:20:53 +0800

    nothing

--- wechatwall/message.js
+++ wechatwall/message.js
@@ -57,7 +57,7 @@ var server = http.createServer(function (request,response){
 					console.log(result);
 					// console.log(result.xml.MsgType[0]);
 					
-					var back = reply(result,'这是一条回复');
+					var back = reply(result,'昨夜若非诸将力战敌军，我命休矣！');
 				    console.log(back);
 					response.end(back);
 				}
