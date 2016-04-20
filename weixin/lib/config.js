320c95a80161c94c42054fedf63f5ca72c8445ca
LCRinesnow <602284158@qq.com>
2016-04-20 13:28:47 +0800

    test

---
+++ weixin/lib/config.js
@@ -0,0 +1,8 @@
+//测试用的微信号，正式使用时请改为实际的appID和appSecret
+module.exports = {
+  appID: 'wx9e3f7f57074f3e22',
+  appSecret: 'c7240daea4ea243d541d39f6627bfc1d',
+  wxPort: 9529,
+  wsPort: 10002,
+  pgPort: 10001
+};
