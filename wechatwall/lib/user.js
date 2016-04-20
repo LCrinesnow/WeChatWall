f905321f281954b537803d485ff67cd4cb5a8de9
LCRinesnow <602284158@qq.com>
2016-04-20 09:58:05 +0800

    index.html/js

--- wechatwall/lib/user.js
+++ wechatwall/lib/user.js
@@ -21,8 +21,10 @@ function getUserInfo(openID){
   return getToken(appID, appSecret).then(function(res){
     var token = res.access_token;
 
-    return new Promise(function(resolve, reject){
+    var userInfo = new Promise(function(resolve, reject){
       request('https://api.weixin.qq.com/cgi-bin/user/info?access_token='+token+'&openid='+openID+'&lang=zh_CN', function(err, res, data){
+          var fuccc = JSON.parse(data);
+          fuccc.access_token = token;
           resolve(JSON.parse(data));
         });
     });
