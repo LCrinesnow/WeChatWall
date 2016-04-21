af66a01cce8142983aa5adbe293846645ccdfc02
LCRinesnow <602284158@qq.com>
2016-04-21 12:09:28 +0800

    test

--- wechatwall/lib/ws.js
+++ wechatwall/lib/ws.js
@@ -1,9 +1,4 @@
-/**
-  这是一个简单的WebSocket服务
-  只提供一个广播的功能，足够微信墙用了
- */
-
-var WS_PORT = require('./config').wsPort;
+var WS_PORT = 10001;
 
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: WS_PORT });
@@ -16,28 +11,12 @@ wss.on('connection', function connection(ws) {
   console.log('new client connected.');
 });
 
-// wss.broadcast = function broadcast(data) {
-//      console.log('------'+ data.xml.MsgType[0]);
-//      console.log(data);
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(JSON.stringify(data));
+  });
+};
 
-     // data="hehe";
-  wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-                  console.log(client.send(data));
-      client.send(data);
-    });
-  };
-
-//   wss.clients.forEach(function each(client) {
-
-//         console.log(JSON.stringify(data));
-//     client.send(JSON.stringify(data));
-//   });
-// };
- 
-// wss.liu = function liu(d){
-//        console.log('liu '+ d);
-// }
 module.exports = {
   wss: wss
 };
