3f2f0568c4f66acedddc93c429356eeac9815e54
LCRinesnow <602284158@qq.com>
2016-04-21 22:54:11 +0800

    test

--- wechatwall/lib/ws.js
+++ wechatwall/lib/ws.js
@@ -13,7 +13,7 @@ wss.on('connection', function connection(ws) {
 
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
-        console.log('========');
+  	    console.log('========');
     client.send(JSON.stringify(data));
   });
 };
