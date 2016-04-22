b107b23c7a10bb8106d90f2c125ecd6dafbc7fb7
LCRinesnow <602284158@qq.com>
2016-04-22 10:49:46 +0800

    wandan

--- wechatwall/lib/user.js
+++ wechatwall/lib/user.js
@@ -21,12 +21,11 @@ function getUserInfo(openID){
   return getToken(appID, appSecret).then(function(res){
     var token = res.access_token;
 
-    var userInfo = new Promise(function(resolve, reject){
+    return new Promise(function(resolve, reject){
       request('https://api.weixin.qq.com/cgi-bin/user/info?access_token='+token+'&openid='+openID+'&lang=zh_CN', function(err, res, data){
-		  resolve(JSON.parse(data));
+          resolve(JSON.parse(data));
         });
     });
-	return userInfo
   }).catch(function(err){
     console.log(err);
   });  
