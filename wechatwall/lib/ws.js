9e4822422b43490392ee9539f040f2f0af08fde0
LCRinesnow <602284158@qq.com>
2016-04-22 12:39:50 +0800

    wandan

--- wechatwall/lib/ws.js
+++ wechatwall/lib/ws.js
@@ -2,6 +2,8 @@ var WS_PORT = 10001;
 
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: WS_PORT });
+console.log(wss);
+console.log(wss.port);
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
@@ -16,11 +18,11 @@ wss.on('connection', function connection(ws) {
 wss.broadcast = function broadcast(data) {
          console.log('========');
 
-          console.log(data);
+          // console.log(data);
 
   wss.clients.forEach(function each(client,data) {
         console.log('++++++');
-        console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
     client.send(JSON.stringify(data));
   });
 };
