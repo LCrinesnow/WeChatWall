9d39ea604e9bce6b5cdf66961e8c89e707949238
LCRinesnow <602284158@qq.com>
2016-06-13 22:45:54 +0800

    改回原来

--- wechatwall/lib/config.js
+++
@@ -1,8 +0,0 @@
-//测试用的微信号，正式使用时请改为实际的appID和appSecret
-module.exports = {
-  appID: 'wx75d11b4f981b1ded',
-  appSecret: '7a915c525f39451f3af19407012459ad',
-  wxPort: 9529,//注意！
-  wsPort: 10002,
-  indexPort:10001
-};
